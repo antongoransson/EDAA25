d02bf3d0a315b69bd189a81c0c82f4d77eaef9e7
antongoransson <anton.goransson95@gmail.com>
2017-09-04 18:03:38 +0200

    a little faster

--- Uppgift2/word/word.c
+++ Uppgift2/word/word.c
@@ -5,8 +5,8 @@
 int main()
 {
 	int c,largest,i,x,k;
-	c=largest=i=0;
-	int *word=malloc(1*sizeof(int));
+	c= largest= i= 0;
+	int *word = malloc(1*sizeof(int));
 	int *a = malloc(1*sizeof(int));
 	while ((c = getchar()) != EOF){
 			while (isalpha(c) > 0){
@@ -17,19 +17,17 @@ int main()
 			}
 			if (i > largest){
 				largest = i;
-				word = realloc(word,(largest+1)*sizeof(int));
-				for (x = 0;x < largest; x++)
-					word[x]=a[x];
+				word = realloc(word,(largest+1)* sizeof(int));
+				for (x = 0; x < largest; x++)
+					word[x]= a[x];
 			}
-			free(a);
-			a = malloc(1*sizeof(int));
-			i=0;
+			a= realloc(a, 1*sizeof(int));
+			i= 0;
 	}
 	free(a);
 	printf("%d characters in longest word: ",largest);
-	for (k = 0; k < largest ; k++){
+	for (k = 0; k < largest; k++)
 		printf("%c", word[k]);
-	}
 	free(word);
 	printf("\n");
 	return 0;
