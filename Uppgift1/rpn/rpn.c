b5e79dd1cd4a2f2f373fc84e8707cbdeecd01180
antongoransson <anton.goransson95@gmail.com>
2017-09-04 17:51:43 +0200

    uppgift 1 +2

---
+++ Uppgift1/rpn/rpn.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int main()
+{
+	int stack[10], top=-1, spec_sign = 0, c, line= 0;
+	while ((c = getchar())!= EOF){
+		while (c != '\n'){
+			if (isdigit(c)){
+				top++;
+				if (top >= 10){
+					spec_sign=2;
+					goto end;
+				} else {
+				stack[top]= c -'0';
+				while (isdigit(c = getchar())){
+					stack[top] = stack[top] * 10 + c-'0';
+					}
+				}
+			} if (c == '+' || c == '-'|| c== '*' || c == '/' ){
+				spec_sign = 1;
+				if (top < 1){
+					spec_sign = 2;
+					goto end;
+				} else {
+					top--;
+					switch (c) {
+						case '+': stack[top]+= stack[top+1]; break;
+						case '-': stack[top]-= stack[top+1]; break;
+						case '*': stack[top]*= stack[top+1]; break;
+						case '/':
+						 if (stack[top+1] == 0){
+							 spec_sign=2;
+							 goto end;
+						 } else {
+							 stack[top]/= stack[top+1]; break;
+						 }
+					}
+				}
+			} else if (c != ' '){
+				if (c!='\n')
+					spec_sign = 2;
+					goto end;
+			}
+			c = getchar();
+		}
+end:
+		line++;
+		if (spec_sign == 0)
+			printf("%s %d%s\n","line",line,": error at \\n");
+		else if (spec_sign == 2){
+			printf("%s %d%s%c\n","line",line,": error at ",c);
+			while ((c = getchar()) != '\n');
+		}
+		else
+			printf("%s%d%s %d\n","line ",line,":", stack[0]);
+		top=-1;
+		spec_sign= 0;
+	}
+	return 0;
+}
---
+++ Uppgift2/word/word.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main()
+{
+	int c,largest,i,x,k;
+	c=largest=i=0;
+	int *word=malloc(1*sizeof(int));
+	int *a = malloc(1*sizeof(int));
+	while ((c = getchar()) != EOF){
+			while (isalpha(c) > 0){
+				a[i] = c;
+				c = getchar();
+				i++;
+				a = realloc(a, (i+1)*sizeof(int));
+			}
+			if (i > largest){
+				largest = i;
+				word = realloc(word,(largest+1)*sizeof(int));
+				for (x = 0;x < largest; x++)
+					word[x]=a[x];
+			}
+			free(a);
+			a = malloc(1*sizeof(int));
+			i=0;
+	}
+	free(a);
+	printf("%d characters in longest word: ",largest);
+	for (k = 0; k < largest ; k++){
+		printf("%c", word[k]);
+	}
+	free(word);
+	printf("\n");
+	return 0;
+}
